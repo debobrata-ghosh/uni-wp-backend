13214f30874ece91f9a2a50c07127ae3d61d0393
Debabrata Ghosh <42384103+debobrata-ghosh@users.noreply.github.com>
2026-04-27 14:05:12 +0530

    acf block

--- wp-content/themes/unitekcollage/inc/blocks-loader.php
+++ wp-content/themes/unitekcollage/inc/blocks-loader.php
@@ -5,15 +5,27 @@ add_action('acf/init', 'mytheme_load_acf_blocks');
 function mytheme_load_acf_blocks() {
     $blocks_dir = get_template_directory() . '/template-parts/blocks/';
 
-    // Scan all subfolders inside blocks/
-    foreach (glob($blocks_dir . '*', GLOB_ONLYDIR) as $block_dir) {
+    if ( ! is_dir( $blocks_dir ) ) {
+        return;
+    }
+
+    /**
+     * Avoid relying on glob(), which can be disabled on some hosts.
+     * Use DirectoryIterator to discover block folders.
+     */
+    foreach ( new DirectoryIterator( $blocks_dir ) as $entry ) {
+        if ( $entry->isDot() || ! $entry->isDir() ) {
+            continue;
+        }
+
+        $block_dir     = $entry->getPathname();
         $register_file = $block_dir . '/register.php';
         $fields_file   = $block_dir . '/fields.php';
 
-        if (file_exists($register_file)) {
+        if ( file_exists( $register_file ) ) {
             include $register_file;
         }
-        if (file_exists($fields_file)) {
+        if ( file_exists( $fields_file ) ) {
             include $fields_file;
         }
     }
